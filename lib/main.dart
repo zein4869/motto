c668f10d25d02148583af02cb422decd6d0cadf9
zein4869 <le.tran.khanh.phuong.6666@gmail.com>
2025-11-07 22:46:48 +0000

    8/11/2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -32,10 +33,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Padding(
 				padding: EdgeInsets.all(5.0),
 				child: Column(
@@ -43,9 +40,25 @@ class _MyHomePageState extends State<MyHomePage> {
 					children: [
 						Expanded(
 							child: TextField(
-								controller: _buf
-							)
-						)
+								controller: _buf,
+                minLines: 2,
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.newline,
+                maxLines: null,
+							) //textfield
+						), //expanded
+            Row(
+              mainAxisAlignment: MainAxisAlignment.End,
+              children: [
+                IconButton(
+                  onPressed: (){
+
+                  },
+                  icon: Icon(Icons.search)
+                )
+              ]
+            ),
+            
 					]
 				)
       )
